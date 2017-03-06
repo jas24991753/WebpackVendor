838d9bbbf967c6cb8c26f8ab15ef5db7ed57f60b
harry <jas24991753@hotmail.com>
2017-03-06 16:13:04 +0800

    第三方插件 TweenMax jquery numeral socket.io state-machine

---
+++ Webpack Vendor/index.js
@@ -0,0 +1,5 @@
+import 'script-loader!./libs/TweenMax.min.js';
+import 'script-loader!./libs/numeral.min.js';
+import 'script-loader!./libs/socket.io.min.js';
+import 'script-loader!./libs/state-machine.min.js';
+import 'script-loader!./libs/jquery.min.js';
